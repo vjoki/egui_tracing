bf49f472a7f1a08ed1c01ce60710d79a74556fc4
vjoki <vjoki@zv.fi>
2024-10-03 19:48:48 +0300

    fix: respect STATIC_MAX_LEVEL

--- egui-tracing/src/ui/components/level_menu_button.rs
+++ egui-tracing/src/ui/components/level_menu_button.rs
@@ -1,4 +1,5 @@
-use egui::{RichText, Ui};
+use egui::{InnerResponse, RichText, Ui};
+use tracing::level_filters::STATIC_MAX_LEVEL;
 
 use super::common::{set_common_props, CommonProps};
 use crate::ui::color::{DEBUG_COLOR, ERROR_COLOR, INFO_COLOR, TRACE_COLOR, WARN_COLOR};
@@ -16,31 +17,48 @@ impl<'a> LevelMenuButton<'a> {
         self
     }
 
-    pub fn show(mut self, ui: &mut Ui) {
+    pub fn show(mut self, ui: &mut Ui) -> bool {
         let state = self.state.as_mut().unwrap();
+        let mut changed = false;
         ui.menu_button("Level", |ui| {
             set_common_props(ui, &self.common_props);
             ui.label("Level Filter");
-            ui.add(egui::Checkbox::new(
-                &mut state.trace,
-                RichText::new("TRACE").color(TRACE_COLOR),
-            ));
-            ui.add(egui::Checkbox::new(
-                &mut state.debug,
-                RichText::new("DEBUG").color(DEBUG_COLOR),
-            ));
-            ui.add(egui::Checkbox::new(
-                &mut state.info,
-                RichText::new("INFO").color(INFO_COLOR),
-            ));
-            ui.add(egui::Checkbox::new(
-                &mut state.warn,
-                RichText::new("WARN").color(WARN_COLOR),
-            ));
-            ui.add(egui::Checkbox::new(
-                &mut state.error,
-                RichText::new("ERROR").color(ERROR_COLOR),
-            ));
+
+            if STATIC_MAX_LEVEL >= tracing::level_filters::LevelFilter::TRACE {
+                changed = changed || ui.add(egui::Checkbox::new(
+                    &mut state.trace,
+                    RichText::new("TRACE").color(TRACE_COLOR),
+                )).changed();
+            }
+
+            if STATIC_MAX_LEVEL >= tracing::level_filters::LevelFilter::DEBUG {
+                changed = changed || ui.add(egui::Checkbox::new(
+                    &mut state.debug,
+                    RichText::new("DEBUG").color(DEBUG_COLOR),
+                )).changed();
+            }
+
+            if STATIC_MAX_LEVEL >= tracing::level_filters::LevelFilter::INFO {
+                changed = changed || ui.add(egui::Checkbox::new(
+                    &mut state.info,
+                    RichText::new("INFO").color(INFO_COLOR),
+                )).changed();
+            }
+
+            if STATIC_MAX_LEVEL >= tracing::level_filters::LevelFilter::WARN {
+                changed = changed || ui.add(egui::Checkbox::new(
+                    &mut state.warn,
+                    RichText::new("WARN").color(WARN_COLOR),
+                )).changed();
+            }
+
+            if STATIC_MAX_LEVEL >= tracing::level_filters::LevelFilter::ERROR {
+                changed = changed || ui.add(egui::Checkbox::new(
+                    &mut state.error,
+                    RichText::new("ERROR").color(ERROR_COLOR),
+                )).changed();
+            }
         });
+        changed
     }
 }
